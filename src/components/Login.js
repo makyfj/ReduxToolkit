67290003b001e0dd0d777c0a135ac105a8b4feb6
Maky <franklinjaradev@gmail.com>
2021-09-06 14:48:22 -0400

    useState

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,20 @@
 import { useDispatch } from "react-redux";
 import { login, logout } from "../features/user";
+import { useState } from "react";
 
 const Login = () => {
   const dispatch = useDispatch();
 
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [email, setEmail] = useState("");
+
   const loginHandler = () => {
     dispatch(
       login({
-        name: "Franklin",
-        age: 24,
-        email: "franklin@gmail.com",
+        name: name,
+        age: age,
+        email: email,
       })
     );
   };
@@ -23,6 +28,30 @@ const Login = () => {
     <div>
       <div>
         <button onClick={loginHandler}>Login</button>
+        <p>
+          Enter your name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </p>
+        <p>
+          Enter your age:
+          <input
+            type="number"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
+        </p>
+        <p>
+          Enter your email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </p>
         <button onClick={logoutHandler}>Logout</button>
       </div>
     </div>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,9 +4,10 @@ const Profile = () => {
   // access the state of features/user
   const user = useSelector((state) => state.user.value);
   const theme = useSelector((state) => state.theme.value);
+
   return (
     <div style={{ color: theme }}>
-      <h1>Profile page</h1>
+      <h1>Profile page: </h1>
       <p>Name: {user.name}</p>
       <p>Age: {user.age}</p>
       <p>Email: {user.email}</p>
